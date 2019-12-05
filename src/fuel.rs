cf6428196a8b8159af0cfe59b69c28a1ae8f154b
Cameron Weinfurt <weinfuc@clarkson.edu>
2019-12-05 10:36:21 -0500

    Part 2 of Day 1

--- src/fuel.rs
+++ src/fuel.rs
@@ -1,1 +1,8 @@
 pub fn calc_from_mass(mass : usize) -> usize { if mass < 6 { 0 } else { mass / 3 - 2 }}
+
+pub fn correct_for_rocket_tyranny(fuel : usize) -> usize {
+    match fuel {
+        0 => 0,
+        _ => fuel + correct_for_rocket_tyranny(calc_from_mass(fuel)),
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,7 @@ fn main() -> std::io::Result<()> {
     let mut fuel_file_contents = String::new();
     fuel_file.read_to_string(&mut fuel_file_contents)?;
 
-    let fuel_needed = fuel_file_contents.lines().filter_map(|x| x.parse::<usize>().ok()).map(calc_from_mass).sum::<usize>();
+    let fuel_needed = fuel_file_contents.lines().filter_map(|x| x.parse::<usize>().ok()).map(calc_from_mass).map(correct_for_rocket_tyranny).sum::<usize>();
     println!("The amount of fuel you need is: {} units", fuel_needed);
 
     Ok(())
