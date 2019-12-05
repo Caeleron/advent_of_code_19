bf0389aac24e1bc45294d4af1b2756ca076fd763
Cameron Weinfurt <weinfuc@clarkson.edu>
2019-12-05 14:43:37 -0500

    Part 2 of Day 2

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,22 @@ fn main() -> std::io::Result<()> {
     let mut prog = String::new();
     prog_file.read_to_string(&mut prog)?;
 
-    println!("The program returned: {}", exec_intcode(parse_to_mem(prog), 0));
+    let mut mem = parse_to_mem(prog);
+
+    mem[1] = 0;
+    mem[2] = 0;
+
+    let slope1 = { let mut mem_copy = mem.to_vec(); mem_copy[1] = 1; exec_intcode(mem_copy, 0) - exec_intcode(mem.to_vec(), 0) };
+
+    let noun = 19690720 / slope1;
+    mem[1] = noun;
+
+    let slope2 = { let mut mem_copy = mem.to_vec(); mem_copy[2] = 1; exec_intcode(mem_copy, 0) - exec_intcode(mem.to_vec(), 0) };
+
+    let zero2 = { let mem_copy = mem.to_vec(); exec_intcode(mem_copy, 0) };
+    let verb = (19690720 - zero2) / slope2;
+
+    println!("Output: {}, Noun: {}, Verb: {}, Solution: {}", exec_intcode(mem, 0), noun, verb, 100 * noun + verb);
 
     Ok(())
 }
